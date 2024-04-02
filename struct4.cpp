9099c041213f14655041faf52f597a150775c47a
Asti22 <uun3461@gmail.com>
2024-04-02 14:18:33 +0700

    menambhkan varibabel mhs[i] untuk nama

--- struct4.cpp
+++ struct4.cpp
@@ -16,26 +16,28 @@ struct Mahasiswa
 };
 int main(){
     Mahasiswa mhs[3];
-    for (int i=0; i<3;i++)
-    cout <<"Nomor Mahasiswa:";
-    getline(cin,mhs.nim);
-    cout <<"Nama Mahasswa:";
-    getline(cin,mhs.nama);
-    cout <<"Alamat Mahasiswa:";
-    cout << "\t nama Desa:";
-    cin >> mhs.alamat.desa;
-    cout <<"\t nama Kota:";
-    cin >> mhs.alamat.kota;
+    for (int i=0; i<3;i++){
+        cout <<"Data ke-"<<(i+1)<<":"<<endl;
+        cout <<"Nomor Mahasiswa:";
+        getline(cin,mhs[i].nim);
+        cout <<"Nama Mahasswa:";
+        getline(cin,mhs[i].nama);
+        cout <<"Alamat Mahasiswa:";
+        cout << "\t nama Desa:";
+        cin >> mhs.alamat.desa;
+        cout <<"\t nama Kota:";
+        cin >> mhs.alamat.kota;
 
-    cout <<"Umur Mahasiswa:";
-    cin>> mhs.umur;
-
-    cout <<endl;
-    cout <<"\n Nim:"<<mhs.nim;
-    cout <<"\n Nama:" <<mhs.nama;
-    cout <<"\n Alamat:";
-    cout <<"\n Desa:"<<mhs.alamat.desa;
-    cout <<"\n Kota:" <<mhs.alamat.kota;
-    cout <<"\n Umur:"<<mhs.umur;
-
+        cout <<"Umur Mahasiswa:";
+        cin>> mhs.umur;
+    }
+    for (int i=0;i<3;i++){
+       cout <<endl;
+       cout <<"\n Nim:"<<mhs.nim;
+       cout <<"\n Nama:" <<mhs.nama;
+       cout <<"\n Alamat:";
+       cout <<"\n Desa:"<<mhs.alamat.desa;
+       cout <<"\n Kota:" <<mhs.alamat.kota;
+       cout <<"\n Umur:"<<mhs.umur;
+    }
 }
