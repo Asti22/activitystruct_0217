bac7f074345b4ebe1cb227fd3f2c9b22f924416f
Asti22 <uun3461@gmail.com>
2024-04-02 14:31:55 +0700

    menambhkan cin ignore untuk baris baru dari cin akan diabaikan dan syntax di bawahnya akan dijalankan

--- struct4.cpp
+++ struct4.cpp
@@ -30,16 +30,19 @@ int main(){
 
         cout <<"Umur Mahasiswa:";
         cin>> mhs[i].umur;
+        cin.ignore(1,'\n');
+        
     }
     for (int i=0;i<3;i++){
         cout <<endl;
         cout <<"Data Mahasiswa ke-"<<(i=1)<<":"<<endl;
        cout <<endl;
-       cout <<"\n Nim:"<<mhs.nim;
-       cout <<"\n Nama:" <<mhs.nama;
+       cout <<"\n Nim:"<<mhs[i].nim;
+       cout <<"\n Nama:" <<mhs[i].nama;
        cout <<"\n Alamat:";
-       cout <<"\n Desa:"<<mhs.alamat.desa;
-       cout <<"\n Kota:" <<mhs.alamat.kota;
-       cout <<"\n Umur:"<<mhs.umur;
+       cout <<"\n\t Desa:"<<mhs[i].alamat.desa;
+       cout <<"\n \tKota:" <<mhs[i].alamat.kota;
+       cout <<"\n Umur:"<<mhs[i].umur;
+       cout << endl;
     }
 }
